3fb30c6e320188e3e802d3718074c7f88ffc5f98
Ryan Desfosses <ryan.desfosses@colorado.edu>
2024-03-18 12:16:07 -0400

    created writer.c file

---
+++ finder-app/writer.c
@@ -0,0 +1,47 @@
+/*
+ * File: writer.c
+ * Author: Ryan Desfosses
+ *
+ * resources:
+ * https://stackoverflow.com/questions/49247612/how-can-i-check-if-the-value-of-argvi-is-null-and-if-it-is-true-end-the-progra
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <errno.h>
+
+
+int main(int argc, char** argv){
+  int fd;
+
+  // check for arguements 
+  if ( argv[1] == NULL ){
+        printf("missing file path");
+        exit(1);
+  }
+
+
+  if ( argv[2] == NULL ){
+      printf("missing write string");
+      exit(1);
+  }
+
+  char *writefile = argv[1];
+  FILE *filename = fopen(writefile, "w");
+  char *writestr = argv[2];
+
+  // write to buffer
+  openlog(NULL, 0, LOG_USER);
+  fprintf(filename, "%s\n", writestr);
+  if (errno == EINTR){
+    syslog(LOG_ERR, "Writing %s to %s", writestr, writefile);
+  } else {
+    syslog(LOG_DEBUG, "Writing %s to %s", writestr, writefile);
+  }
+
+
+
+}
